e13539e661177b3c1485a6b37b52b35e30cac5b6
Ali Helmi <ahelmi365@gmail.com>
2024-06-05 19:12:57 +0300

    add browser router to main

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./styles/theme.ts";
+import { BrowserRouter } from "react-router-dom";
+import "./styles/style.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
