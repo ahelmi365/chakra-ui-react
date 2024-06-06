bbb0a018326458a9e95fc6adcd9f420d9f77828b
Ali Helmi <ahelmi365@gmail.com>
2024-06-06 14:23:24 +0300

    add Drawer

---
+++ src/components/ChakraDrawer.tsx
@@ -0,0 +1,104 @@
+import { AddIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  FormLabel,
+  Heading,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  InputRightAddon,
+  Select,
+  Stack,
+  Textarea,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useRef } from "react";
+const ChakraDrawer = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const firstField = useRef(null);
+
+  return (
+    <Stack gap={4}>
+      <Heading size={"md"}>Chakra-UI Drawer </Heading>
+      <Button
+        leftIcon={<AddIcon />}
+        colorScheme="primary"
+        onClick={onOpen}
+        w={"fit-content"}
+      >
+        Create user
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        initialFocusRef={firstField}
+        onClose={onClose}
+        size={"sm"}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader borderBottomWidth="1px">
+            Create a new account
+          </DrawerHeader>
+
+          <DrawerBody>
+            <Stack spacing="24px">
+              <Box>
+                <FormLabel htmlFor="username">Name</FormLabel>
+                <Input
+                  ref={firstField}
+                  id="username"
+                  placeholder="Please enter user name"
+                />
+              </Box>
+
+              <Box>
+                <FormLabel htmlFor="url">Url</FormLabel>
+                <InputGroup>
+                  <InputLeftAddon>http://</InputLeftAddon>
+                  <Input
+                    type="url"
+                    id="url"
+                    placeholder="Please enter domain"
+                  />
+                  <InputRightAddon>.com</InputRightAddon>
+                </InputGroup>
+              </Box>
+
+              <Box>
+                <FormLabel htmlFor="owner">Select Owner</FormLabel>
+                <Select id="owner" defaultValue="segun">
+                  <option value="segun">Segun Adebayo</option>
+                  <option value="kola">Kola Tioluwani</option>
+                </Select>
+              </Box>
+
+              <Box>
+                <FormLabel htmlFor="desc">Description</FormLabel>
+                <Textarea id="desc" />
+              </Box>
+            </Stack>
+          </DrawerBody>
+
+          <DrawerFooter borderTopWidth="1px">
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Submit</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </Stack>
+  );
+};
+
+export default ChakraDrawer;
