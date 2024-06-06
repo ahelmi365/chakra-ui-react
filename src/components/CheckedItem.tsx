f5560fea72ce1038d336d6508fb235ba7a4d56ba
Ali Helmi <ahelmi365@gmail.com>
2024-06-06 14:24:49 +0300

    add color prop to icon

--- src/components/CheckedItem.tsx
+++ src/components/CheckedItem.tsx
@@ -2,11 +2,12 @@ import { CheckCircleIcon } from "@chakra-ui/icons";
 import { HStack, Text } from "@chakra-ui/react";
 interface Props {
   text: string;
+  color: string;
 }
-const CheckedItem = ({ text }: Props) => {
+const CheckedItem = ({ text, color }: Props) => {
   return (
     <HStack textAlign={["left", "left", "center"]}>
-      <CheckCircleIcon color="#652CD3" />
+      <CheckCircleIcon color={color} />
       <Text textAlign={"left"}>{text}</Text>
     </HStack>
   );
