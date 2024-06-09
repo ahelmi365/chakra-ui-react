40272d10fb811478313ca27630d8c405ebea26dc
Ali Helmi <ahelmi365@gmail.com>
2024-06-09 13:22:46 +0300

    add landing page

---
+++ src/components/Landing.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+import Header from "./Header";
+import Pricing from "./Pricing";
+
+const Landing = () => {
+  return (
+    <Box>
+      <Header />
+      <Pricing />
+    </Box>
+  );
+};
+
+export default Landing;
