cf112287b404bd5edfcb66653f65ecfef5ccc6cb
Ali Helmi <ahelmi365@gmail.com>
2024-06-09 18:38:02 +0300

    re-order imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import theme from "./styles/theme.ts";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
 import "./styles/fontFaces.css";
+import theme from "./styles/theme.ts";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
