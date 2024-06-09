c9b95216a14bd6603b6bc3cb60ed858920cb041c
Ali Helmi <ahelmi365@gmail.com>
2024-06-09 16:44:35 +0300

    rename style.css to fontFaces.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./styles/theme.ts";
 import { BrowserRouter } from "react-router-dom";
-import "./styles/style.css";
+import "./styles/fontFaces.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
