6ac92b1f8ab8ab38639a9850774dda77fe4d570b
Ali Helmi <ahelmi365@gmail.com>
2024-06-10 14:01:17 +0300

    change font size

--- src/components/ChakraModal.tsx
+++ src/components/ChakraModal.tsx
@@ -36,7 +36,7 @@ const ChakraModal = () => {
             <ModalHeader>Modal Title</ModalHeader>
             <ModalCloseButton />
             <ModalBody>
-              <Text>
+              <Text fontSize={"sm"} fontWeight={"normal"}>
                 Sit nulla est ex deserunt exercitation anim occaecat. Nostrud
                 ullamco deserunt aute id consequat veniam incididunt duis in
                 sint irure nisi. Mollit officia cillum Lorem ullamco minim
