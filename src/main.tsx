c274fb18f90b973e155906f14036ab814158b211
Ali Helmi <ahelmi365@gmail.com>
2024-06-11 08:47:22 +0300

    update imports

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
-import "./styles/fontFaces.css";
-import theme from "./styles/theme.ts";
+import "./theme/fontFaces.css";
+import theme from "./theme";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
