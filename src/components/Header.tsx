389e1669611ff5ebd71218afc68e8750ae3ff43b
Ali Helmi <ahelmi365@gmail.com>
2024-06-11 17:06:58 +0300

    change text color in dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { Box, Heading, Text, useColorModeValue } from "@chakra-ui/react";
 
 const Header = () => {
   const bgColor = useColorModeValue("primary.500", "primary.400");
+  const textColor = useColorModeValue("gray.50", "gray.700");
   return (
     <Box
       color={"white"}
@@ -12,10 +13,19 @@ const Header = () => {
       textAlign={["left", "left", "center"]}
       as="section"
     >
-      <Heading fontWeight={"extrabold"} fontSize={["3xl", "3xl", "5xl"]}>
+      <Heading
+        fontWeight={"extrabold"}
+        fontSize={["3xl", "3xl", "5xl"]}
+        color={textColor}
+      >
         Chakra UI
       </Heading>
-      <Text fontWeight="medium" fontSize={["lg", "lg", "2xl"]} pt="4">
+      <Text
+        fontWeight="medium"
+        fontSize={["lg", "lg", "2xl"]}
+        pt="4"
+        color={textColor}
+      >
         Build Accessible React Apps with Speed ⚡️
       </Text>
     </Box>
