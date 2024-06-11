f17f1233f9a29e894ca9f27fdf3f386adccca264
Ali Helmi <ahelmi365@gmail.com>
2024-06-11 09:21:56 +0300

    use hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
-import "./theme/fontFaces.css";
 import theme from "./theme";
+import "./theme/fontFaces.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ChakraProvider>
   </React.StrictMode>
 );
