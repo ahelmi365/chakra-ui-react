58e78f552226d23fe1d47fe9c154c3c534981208
Ali Helmi <ahelmi365@gmail.com>
2024-06-12 10:04:50 +0300

    add chakra alert

---
+++ src/components/ChakraAlert.tsx
@@ -0,0 +1,18 @@
+import { Alert, AlertIcon, Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
+interface Props {
+  status: "info" | "warning" | "success" | "error" | "loading" | undefined;
+  children: ReactNode;
+}
+const ChakraAlert = ({ status, children }: Props) => {
+  return (
+    <Alert status={status} rounded={8}>
+      <AlertIcon />
+      <Box fontWeight={"bold"} p={2} fontSize={"large"}>
+        {children}
+      </Box>
+    </Alert>
+  );
+};
+
+export default ChakraAlert;
